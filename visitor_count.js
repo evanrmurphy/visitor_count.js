b861765f3037c241deb84f01728946660997bb41
Evan R. Murphy <evanrmurphy@gmail.com>
2013-09-20 23:52:15 -0500

    Refactoring

--- visitor_count.js
+++ visitor_count.js
@@ -1,22 +1,33 @@
 // visitor_count.js: Simple node server with variable
-// persistent across requests.
+// persistent across requests and server restarts.
+
 (function() {
+
   var 
     fs   = require('fs');
+    f    = 'n.txt'
     http = require('http');
     n    = 0;
 
-  console.log('loading...');
-  fs.readFile('n.txt', {encoding: 'utf8'}, function(err, data) { 
-    if (err) throw err;
+  var start = function(data) {
     n = parseInt(data); 
-
     http.createServer(function (req, res) {
       n += 1;
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end( 'You are visitor #'+n+', congratulations!' );
-      fs.writeFile('n.txt', n, function(err){ if (err) throw err; });
+      fs.writeFile(f, n, function(err){ if (err) throw err; });
     }).listen(1337, '127.0.0.1');
     console.log('Server running at http://127.0.0.1:1337/');
-  });
+  };
+
+  var load = function() {
+    console.log('loading...');
+    fs.readFile(f, {encoding: 'utf8'}, function(err, data) { 
+      if (err) throw err;
+      start(data);
+    });
+  };
+
+  load();
+
 }).call(this);
