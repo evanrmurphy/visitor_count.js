0434d5bff2315431f5dde67b851299856966d4bb
Evan R. Murphy <evanrmurphy@gmail.com>
2013-09-20 22:59:34 -0500

    Dev version serves at http://localhost:1337/

--- visitor_count.js
+++ visitor_count.js
@@ -2,9 +2,9 @@
 // persistent across requests.
 var n = 0;
 var http = require('http');
-var port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
   n += 1;
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end( 'You are visitor #'+n+', congratulations!' );
-}).listen(port);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
