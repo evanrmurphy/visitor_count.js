b90b23c872b9240894c6b686a31686c8431641ce
Evan R. Murphy <evanrmurphy@gmail.com>
2013-09-20 23:08:44 -0500

    Persistence! Load file 'n.txt' into variable at server start, write to it on each request.

--- visitor_count.js
+++ visitor_count.js
@@ -1,10 +1,20 @@
 // visitor_count.js: Simple node server with variable
 // persistent across requests.
-var n = 0;
-var http = require('http');
-http.createServer(function (req, res) {
-  n += 1;
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end( 'You are visitor #'+n+', congratulations!' );
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var 
+  fs   = require('fs');
+  http = require('http');
+  n    = 0;
+
+console.log('loading...');
+fs.readFile('n.txt', {encoding: 'utf8'}, function(err, data) { 
+  if (err) throw err;
+  n = parseInt(data); 
+
+  http.createServer(function (req, res) {
+    n += 1;
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end( 'You are visitor #'+n+', congratulations!' );
+    fs.writeFile('n.txt', n, function(err){ if (err) throw err; });
+  }).listen(1337, '127.0.0.1');
+  console.log('Server running at http://127.0.0.1:1337/');
+});
